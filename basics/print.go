c2d929bd244c5d12cde4347b0a2259272820b903
Ritvik Mukherjee <mritvikcode1010@gmail.com>
2026-06-10 19:38:34 +0530

    redundancy reduced

--- basics/print.go
+++ basics/print.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hi!")
+
+	fmt.Println("Hi")
+	fmt.Println("ritvik")
+
+	congrats := "Happy birthday ritvik"
+	fmt.Println(congrats)
+
+	number := 34
+	fmt.Printf("The type is %T\n", number)
+
+	val1, val2 := 35.2, "Hello again ritvik"
+	println(val1, val2)
 }
